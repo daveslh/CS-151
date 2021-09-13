b834fbd2487e1eda6fbac258be03adb26f99552c
daveslh <60028526+daveslh@users.noreply.github.com>
2021-09-12 23:21:27 -0700

    First commit. Created Java class Species with instance variables and a constructor that initializes them

---
+++ src/animals/Species.java
@@ -0,0 +1,24 @@
+package animals;
+
+import java.util.*;
+
+public class Species {
+	private String name;
+	private double weight, length, height;
+	private List<Species> diet;
+	
+	public Species() {
+		name = "INPUT SPECIES NAME";
+		weight = 0;
+		length = 0;
+		height = 0;
+		diet = new ArrayList<Species>();
+		
+	}
+
+/** Starter class file for Species class, which holds basic information (like name,
+ * weight, length, and height) about the species, a constructor that initializes the variables, as well as an ArrayList of type
+ * species that holds what its diet consists of (other species objects).
+ */
+
+}
